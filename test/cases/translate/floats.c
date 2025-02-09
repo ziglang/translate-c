bfb79f554e5a9e0eeceba671a75b8eaaba42129e
Elliot Wannemacher <ewannemacher13@gmail.com>
2025-02-09 10:56:16 -0500

    translate floats

--- test/cases/translate/floats.c
+++ test/cases/translate/floats.c
@@ -4,9 +4,8 @@ int c = 3.1415;
 double d = 3;
 
 // translate
-// expect=fail
 //
-// pub export var a: f32 = @as(f32, @floatCast(3.1415));
+// pub export var a: f32 = @floatCast(3.1415);
 // pub export var b: f64 = 3.1415;
-// pub export var c: c_int = @as(c_int, @intFromFloat(3.1415));
+// pub export var c: c_int = @intFromFloat(3.1415);
 // pub export var d: f64 = 3;
