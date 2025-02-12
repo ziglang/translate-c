042f20dd1ba3a85b50a683b5ea31ae0cc9bc2011
Carter Snook <cartersnook04@gmail.com>
2025-02-11 20:28:02 -0600

    ensure all casts have a result type
    Some casts would not have a result type emitted. This fixes the issue
    and adds onto an existing test to maintain the correct behavior.

--- test/cases/translate/c_style_cast.c
+++ test/cases/translate/c_style_cast.c
@@ -2,10 +2,21 @@ int int_from_float(float a) {
     return (int)a;
 }
 
+int add_int_from_float(float a, float b) {
+    return (int)a + (int) b;
+}
+
 // translate
 //
 // pub export fn int_from_float(arg_a: f32) c_int {
 //     var a = arg_a;
 //     _ = &a;
 //     return @intFromFloat(a);
+// }
+// pub export fn add_int_from_float(arg_a: f32, arg_b: f32) c_int {
+//     var a = arg_a;
+//     _ = &a;
+//     var b = arg_b;
+//     _ = &b;
+//     return @as(c_int, @intFromFloat(a)) + @as(c_int, @intFromFloat(b));
 // }
