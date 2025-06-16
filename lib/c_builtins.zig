7e2f76256609444a9ea3b10ad6ca69c2179df896
Veikka Tuominen <git@vexu.eu>
2025-06-16 17:16:21 +0300

    add ___builtin_expect and enable handle_assert.h test

--- lib/c_builtins.zig
+++ lib/c_builtins.zig
@@ -72,6 +72,13 @@ pub inline fn exp(val: f64) f64 {
     return @exp(val);
 }
 
+/// The return value of __builtin_expect is `expr`. `c` is the expected value
+/// of `expr` and is used as a hint to the compiler in C. Here it is unused.
+pub inline fn expect(expr: c_long, c: c_long) c_long {
+    _ = c;
+    return expr;
+}
+
 pub inline fn fabsf(val: f32) f32 {
     return @abs(val);
 }
