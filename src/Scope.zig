2d9738ab346758dd72bc64e948c1dc8c432c02a4
Carmen <carmen@dotcarmen.dev>
2025-10-28 21:50:21 +0100

    use std.mem.trimEnd

--- src/Scope.zig
+++ src/Scope.zig
@@ -286,10 +286,7 @@ pub const Root = struct {
             var count: u32 = 0;
             for (members.member_fns.items) |func| {
                 const func_name = func.data.name.?;
-                var func_name_trimmed = func_name;
-                while (std.mem.endsWith(u8, func_name_trimmed, "_"))
-                    func_name_trimmed = func_name_trimmed[0 .. func_name_trimmed.len - 1];
-
+                const func_name_trimmed = std.mem.trimEnd(u8, func_name, "_");
                 const last_index = std.mem.lastIndexOf(u8, func_name_trimmed, "_");
                 const last_name = if (last_index) |index| func_name[index + 1 ..] else continue;
                 var same_count: u32 = 0;
